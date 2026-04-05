46f47010152c457e0e04098af2fea17bc9ce0056
dasiloy <oyewoledamilare00@gmail.com>
2026-04-05 04:10:41 +0100

    fix: change default PORT value from 3000 to 4000 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -93,7 +93,7 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0', () => {
     logger.log(`Running on port ${port}`);
   });
